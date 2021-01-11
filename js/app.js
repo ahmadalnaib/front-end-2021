96fd50b89e5b39f658c4ce597465ec134c381b2a
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-11 13:00:43 +0100

    learn about get data from api

--- js/app.js
+++ js/app.js
@@ -9,13 +9,45 @@ function loadData()
 {
   const xhr=new XMLHttpRequest();
 
-  xhr.open('get','data.txt',true);
+  //xhr.open('get','data.txt',true);
+  xhr.open('GET','https://api.github.com/users',true)
   xhr.onload=function(){
     if(this.status === 200)
     {
-      data.innerHTML=`
-      <h1>${this.responseText}</h1>
-      `
+      //TEXT DATA
+
+      // data.innerHTML=`
+      // <h1>${this.responseText}</h1>
+      // `
+
+//JSON  ONE DATA
+
+      // const data=JSON.parse(this.responseText);
+      // const output=`
+      // <ul>
+      // <li>${data.id}</li>
+      // <li>${data.name}</li>
+      // </ul>
+      // `
+      // document.getElementById('data').innerHTML=output
+
+
+
+      // JSON MANY DATA
+      const data=JSON.parse(this.responseText);
+
+      let output='';
+      data.forEach(function(d){
+       output += `
+       <p> id :${d.id}</p>
+       <p> name :${d.login}</p>
+       <a target="_blank" href=${d.url}>Vist</a>
+
+       `
+      });
+
+      document.getElementById('data').innerHTML=output;
+
     }
   }
   xhr.send();
