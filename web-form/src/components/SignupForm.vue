59c8f7fe90069114ebc597ea6729b3a66f7a45df
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-13 16:47:44 +0100

    two data binding select checkboxes

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,13 +1,58 @@
 <template>
   <form action="">
     <label >Email</label>
-    <input type="email" required>
+    <input type="email" required v-model="email">
+
+    <label >Password</label>
+    <input type="password" required v-model="password">
+
+    <label >Role:</label>
+    <select v-model="role">
+      <option selected value="developer">developer</option>
+      <option value="designer">designer</option>
+    </select>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms">
+      <label>Accept terms and condition</label> 
+    </div>
+
+    <div>
+      <input type="checkbox" value="ahmed" v-model="names">
+      <label for="">ahmed</label>
+    </div>
+     <div>
+      <input type="checkbox" value="ato" v-model="names">
+      <label for="">ato</label>
+    </div>
+     <div>
+      <input type="checkbox" value="vieri" v-model="names">
+      <label for="">vieri</label>
+    </div>
   </form>
+
+  <p> EMAIL: {{email}}</p>
+  <p>Password: {{password}}</p>
+  <p>Role: {{role}}</p>
+  <p>Terms: {{terms}}</p>
+  <ul>
+    <li>{{names}}</li>
+  </ul>
 </template>
 
 <script>
 export default {
+data(){
+  return {
+ email:'',
+ password:'',
+ role:'',
+ terms:false,
+ names:[],
 
+
+  }
+}
 }
 </script>
 
@@ -33,7 +78,7 @@ label {
 
 }
 
-input{
+input,select{
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -44,5 +89,14 @@ input{
 
 input:focus{
   outline-color: aqua;
+}
+
+input[type="checkbox"]
+{
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
