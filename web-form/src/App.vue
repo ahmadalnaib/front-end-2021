7af60d6539c9dcec10735d48d4e768feac201bce
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-13 16:05:24 +0100

    form-vue-project

---
+++ web-form/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+<signup-form/>
+
+</template>
+
+<script>
+
+import SignupForm from './components/SignupForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+  
+    SignupForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+ 
+  background: #eee;
+
+}
+</style>
---
+++ web-form/src/components/SignupForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form action="">
+    <label >Email</label>
+    <input type="email" required>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: #fff;
+  text-align: left ;
+  padding: 40px;
+  border-radius: 10px;
+
+}
+
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 1.6rem;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+
+}
+
+input{
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+
+input:focus{
+  outline-color: aqua;
+}
+</style>
